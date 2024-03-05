5bb27a5b9c7c1ec5f8fe4c3c2299b1f38f5b208a
saouatocto <101819180+saouatocto@users.noreply.github.com>
2024-03-05 15:24:58 +0100

    Setup folder structure & create PageWrapper component (#1)
    * add typescript
    
    * feat: add user listing page & wrapper component

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+import { UserListingPage } from "pages";
+
+function App() {
+  return (
+    <div className="App">
+        <UserListingPage />
+    </div>
+  );
+}
+
+export default App;
